855f08b5d6858420959fae09b25ef01e836c8035
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-02-28 12:03:21 +0300

    28.02d-c6

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -2,159 +2,191 @@
   <section class="questions__section">
     <div class="container">
       <div class="row question_box">
-        <div class="col-8 question_text__box">
+        <div class="question_text__box">
           <h3 class="question_text_box__title">Часто задаваемые вопросы</h3>
-
-          <div class="accordion" id="accordionExample">
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingOne">
-                <button
-                  class="accordion-button"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseOne"
-                  aria-expanded="true"
-                  aria-controls="collapseOne"
-                >
-                  Как настроить часто задаваемые вопросы в MUNIS ?
-                </button>
-              </h2>
-              <div
-                id="collapseOne"
-                class="accordion-collapse collapse show"
-                aria-labelledby="headingOne"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  8 октября 2020 года Совет директоров Всемирного банка одобрил
-                  проект «Модернизация национальной инновационной системы
-                  Узбекистана» с предоставлением финансовых средств в размере 50
-                  млн. долларов США. Исполнительным органом данного проекта
-                  является Министерство Инновационного развития (МИР) Республики
-                  Узбекистан.
+          <div class="row question_text_box_row">
+            <div class="col-8 question_according__box my-4">
+              <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingOne">
+                    <button
+                      class="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseOne"
+                      aria-expanded="true"
+                      aria-controls="collapseOne"
+                    >
+                      Как настроить часто задаваемые вопросы в MUNIS ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseOne"
+                    class="accordion-collapse collapse show"
+                    aria-labelledby="headingOne"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingTwo">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseTwo"
+                      aria-expanded="false"
+                      aria-controls="collapseTwo"
+                    >
+                      Что мы можем вам порекомендовать ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseTwo"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingTwo"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingFour">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFour"
+                      aria-expanded="false"
+                      aria-controls="collapseFour"
+                    >
+                      С какими компаниями у нас отношения ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFour"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingFour"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingFive">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseFive"
+                      aria-expanded="false"
+                      aria-controls="collapseFive"
+                    >
+                      Как настроить часто задаваемые вопросы в MUNIS ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseFive"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingFive"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingSix">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseSix"
+                      aria-expanded="false"
+                      aria-controls="collapseSix"
+                    >
+                      С какими компаниями у нас отношения ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseSix"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingSix"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingFive">
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target="#collapseThree"
+                      aria-expanded="false"
+                      aria-controls="collapseThree"
+                    >
+                      Как настроить часто задаваемые вопросы в MUNIS ?
+                    </button>
+                  </h2>
+                  <div
+                    id="collapseThree"
+                    class="accordion-collapse collapse"
+                    aria-labelledby="headingFive"
+                    data-bs-parent="#accordionExample"
+                  >
+                    <div class="accordion-body">
+                      8 октября 2020 года Совет директоров Всемирного банка
+                      одобрил проект «Модернизация национальной инновационной
+                      системы Узбекистана» с предоставлением финансовых средств
+                      в размере 50 млн. долларов США. Исполнительным органом
+                      данного проекта является Министерство Инновационного
+                      развития (МИР) Республики Узбекистан.
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingTwo">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseTwo"
-                  aria-expanded="false"
-                  aria-controls="collapseTwo"
-                >
-                  Что мы можем вам порекомендовать ?
-                </button>
-              </h2>
-              <div
-                id="collapseTwo"
-                class="accordion-collapse collapse"
-                aria-labelledby="headingTwo"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  8 октября 2020 года Совет директоров Всемирного банка одобрил
-                  проект «Модернизация национальной инновационной системы
-                  Узбекистана» с предоставлением финансовых средств в размере 50
-                  млн. долларов США. Исполнительным органом данного проекта
-                  является Министерство Инновационного развития (МИР) Республики
-                  Узбекистан.
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingThree">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  С какими компаниями у нас отношения ?
-                </button>
-              </h2>
-              <div
-                id="collapseThree"
-                class="accordion-collapse collapse"
-                aria-labelledby="headingThree"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  8 октября 2020 года Совет директоров Всемирного банка одобрил
-                  проект «Модернизация национальной инновационной системы
-                  Узбекистана» с предоставлением финансовых средств в размере 50
-                  млн. долларов США. Исполнительным органом данного проекта
-                  является Министерство Инновационного развития (МИР) Республики
-                  Узбекистан.
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingThree">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  Как настроить часто задаваемые вопросы в MUNIS ?
-                </button>
-              </h2>
-              <div
-                id="collapseThree"
-                class="accordion-collapse collapse"
-                aria-labelledby="headingThree"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  8 октября 2020 года Совет директоров Всемирного банка одобрил
-                  проект «Модернизация национальной инновационной системы
-                  Узбекистана» с предоставлением финансовых средств в размере 50
-                  млн. долларов США. Исполнительным органом данного проекта
-                  является Министерство Инновационного развития (МИР) Республики
-                  Узбекистан.
-                </div>
-              </div>
-            </div>
-            <div class="accordion-item">
-              <h2 class="accordion-header" id="headingThree">
-                <button
-                  class="accordion-button collapsed"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#collapseThree"
-                  aria-expanded="false"
-                  aria-controls="collapseThree"
-                >
-                  С какими компаниями у нас отношения ?
-                </button>
-              </h2>
-              <div
-                id="collapseThree"
-                class="accordion-collapse collapse"
-                aria-labelledby="headingThree"
-                data-bs-parent="#accordionExample"
-              >
-                <div class="accordion-body">
-                  8 октября 2020 года Совет директоров Всемирного банка одобрил
-                  проект «Модернизация национальной инновационной системы
-                  Узбекистана» с предоставлением финансовых средств в размере 50
-                  млн. долларов США. Исполнительным органом данного проекта
-                  является Министерство Инновационного развития (МИР) Республики
-                  Узбекистан.
-                </div>
-              </div>
+            <div class="col-4 question_img__box">
+              <img src="./../../public/images/Questions/frame.png" alt="" />
             </div>
           </div>
-        </div>
-        <div class="col-4 question_img__box">
-          <img src="./../../public/images/Questions/frame.png" alt="" />
         </div>
       </div>
     </div>
@@ -187,5 +219,9 @@ export default {
 }
 .question_text_box__question {
   font-weight: 500;
+}
+.question_text_box_row {
+  /* align-items: center; */
+  justify-content: space-between;
 }
 </style>
