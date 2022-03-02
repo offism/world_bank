0688f2404c6e47c71b258696f09f18e13b62303c
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-02 10:56:18 +0300

    02.03d-c1

---
+++ src/components/Map.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="map__section">
+    <div class="container">
+      <div class="map__wrapper">
+        <div class="row">
+          <div class="col-5">
+            <label class="map__title h5" id="map__adress">Адрес:</label>
+            <small class="map_adress__text" for="map__adress"
+              >Адрес: 100174, г. Ташкент, Алмазарский р-н, ул. Университетская,
+              дом 7</small
+            >
+            <label class="map__title fmt_top h5" id="map__phone"
+              >Телефон:</label
+            >
+            <small class="map_adress__text" for="map__phone"
+              >+998 97 170 67 84</small
+            >
+            <label class="map__title h5 fmt_top" id="map__email"
+              >Эл. адрес:</label
+            >
+            <small class="map_adress__text" for="map__email"
+              >munis@mininnovation.uz</small
+            >
+            <ul class="map_socials">
+              <li>
+                <a href="#" class="f"><i class="fab fa-facebook-f"></i></a>
+              </li>
+              <li>
+                <a href="#" class="i"><i class="fab fa-instagram"></i></a>
+              </li>
+              <li>
+                <a href="#" class="t"><i class="fab fa-telegram"></i></a>
+              </li>
+              <li>
+                <a href="#" class="y"><i class="fab fa-youtube"></i></a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-7">
+            <img src="./../../public/images/map/map.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "MapSection",
+};
+</script>
+
+<style scoped></style>
--- src/pages/contacts/contacts.vue
+++ src/pages/contacts/contacts.vue
@@ -2,6 +2,8 @@
   <div>
     <NavbarSection />
     <PaginationTitle />
+    <MapSection />
+
     <Footer />
   </div>
 </template>
@@ -10,12 +12,14 @@
 import NavbarSection from "./../../components/NavbarSection.vue";
 import Footer from "./../../components/Footer.vue";
 import PaginationTitle from "./../../components/PaginationTitle.vue";
+import MapSection from "./../../components/Map.vue";
 
 export default {
   name: "Contacts",
   components: {
     NavbarSection,
     Footer,
+    MapSection,
     PaginationTitle,
   },
 };
