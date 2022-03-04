254357dd11b85a327dddef2809907d42a082c847
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2022-03-04 18:14:43 +0300

    04.03d-c11

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,7 +4,7 @@
       <div class="map__wrapper">
         <h3>Контакты</h3>
         <div class="row">
-          <div class="col-5">
+          <div class="col-md-5 col-sm-12">
             <label class="map__title h5" id="map__adress">Адрес:</label>
             <small class="map_adress__text" for="map__adress"
               >Адрес: 100174, г. Ташкент, Алмазарский р-н, ул. Университетская,
@@ -37,7 +37,7 @@
               </li>
             </ul>
           </div>
-          <div class="col-7">
+          <div class="col-md-7 col-sm-12">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2994.1072610887645!2d69.34439151504266!3d41.37175427926567!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38aef39a116bbcbf%3A0xa31397ff937bc465!2z0KPQvdC40LLQtdGA0YHQuNGC0LXRgtGB0LrQsNGPINGD0LvQuNGG0LAsINCi0L7RiNC60LXQvdGCLCDQo9C30LHQtdC60LjRgdGC0LDQvQ!5e0!3m2!1sru!2s!4v1646208334087!5m2!1sru!2s"
               width="100%"
